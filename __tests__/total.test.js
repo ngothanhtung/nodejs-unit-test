ad150af72e90eef5e052eaf5da9aa7d43ab4b094
Ngô Thanh Tùng <ngothanhtung.it@outlook.com>
2023-06-15 17:15:32 +0700

    Create total.test.js

---
+++ __tests__/total.test.js
@@ -0,0 +1,8 @@
+// import from the sum.js file
+const sum = require('../src/total');
+
+describe('math', () => {
+  test('adds 1 + 2 + 3 to equal 6', () => {
+    expect(total(1, 2, 3)).toBe(6);
+  });
+});
